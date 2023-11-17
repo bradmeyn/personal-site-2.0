c9c3c758d4ec8a5df8fa07d5d84f97ee334ab420
bradmeyn <bradjmeyn@gmail.com>
2023-11-17 22:16:41 +1100

    update color

--- src/data/links.ts
+++ src/data/links.ts
@@ -1,5 +1,19 @@
-export const links = [
-  { name: "Github", url: "", icon: "fab fa-github" },
-  { name: "Linkedin", url: "", icon: "fab fa-linkedin" },
-  { name: "Codepen", url: "", icon: "fab fa-codepen" },
+export const externalLinks = [
+  { name: "GitHub", url: "https://github.com/bradmeyn", icon: "fab fa-github" },
+  {
+    name: "Linkedin",
+    url: "https://www.linkedin.com/in/bradmeyn/",
+    icon: "fab fa-linkedin",
+  },
+  {
+    name: "Codepen",
+    url: "https://codepen.io/bradmeyn",
+    icon: "fab fa-codepen",
+  },
+];
+
+export const navLinks = [
+  { name: "About", url: "/about" },
+  { name: "Projects", url: "/projects" },
+  { name: "Contact", url: "/contact" },
 ];
--- src/scripts/index.js
+++ src/scripts/index.js
@@ -22,3 +22,55 @@ const fadeElements = document.querySelectorAll(".animated--fade-in");
 fadeElements.forEach((element) => {
   observer.observe(element);
 });
+
+function toggleMenu() {
+  document.body.classList.toggle("navbar--active");
+}
+
+function deactivateMenu() {
+  document.body.classList.remove("navbar--active");
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  // Get the header after the DOM is fully loaded
+  const header = document.querySelector("header");
+  const hamburger = document.querySelector(".hamburger");
+  const nav = document.querySelector(".navbar");
+  const navLinks = document.querySelectorAll(".nav__link");
+
+  hamburger?.addEventListener("click", toggleMenu);
+
+  // Close navbar when a nav link is clicked
+  navLinks.forEach((link) => {
+    link.addEventListener("click", deactivateMenu);
+  });
+
+  // Close navbar when clicking outside
+  document.addEventListener("click", (e) => {
+    const isClickInsideNav =
+      nav?.contains(e.target) || hamburger?.contains(e.target);
+
+    if (!isClickInsideNav) {
+      deactivateMenu();
+    }
+  });
+
+  // // Background color change on scroll
+  window.addEventListener("scroll", () => {
+    if (header) {
+      if (window.scrollY > 0) {
+        header.classList.add("header--scrolled");
+      } else {
+        header.classList.remove("header--scrolled");
+      }
+    }
+  });
+
+  // Close mobile menu on resize
+  window.addEventListener("resize", () => {
+    if (window.innerWidth > 760) {
+      hamburger?.classList.remove("hamburger--active");
+      nav?.classList.remove("navbar--active");
+    }
+  });
+});
