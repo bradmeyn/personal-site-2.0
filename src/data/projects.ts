6084010ae55bed8d7e442dd459cd64626c82bf11
bradmeyn <bradjmeyn@gmail.com>
2024-01-06 16:57:44 +1100

    Add featured and not featured projects

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -1,12 +1,12 @@
 export interface Project {
   title: string;
   description: string;
-
+  featured: boolean;
   link: string;
   tech: string[];
   repo: string;
   mobileImage?: string;
-  desktopImage: string;
+  desktopImage?: string;
 }
 
 export const projects = [
@@ -17,32 +17,15 @@ export const projects = [
     tech: ["SvelteKit", "Tailwind", "Typescript", "Node"],
     desktopImage: "/assets/wealthkit-desktop.jpg",
     mobileImage: "/assets/wealthkit-mobile.jpg",
-    link: "https://wealthkit-git-main-bradmeyn.vercel.app/",
+    link: "https://bradmeyn-wealthkit.vercel.app/",
     repo: "https://github.com/bradmeyn/wealthkit",
+    featured: true,
   },
-  {
-    title: "Workshop",
-    description: "A trello-esq kanban app for managing side-project tasks. ",
-    tech: ["React", "Typescript", "Tailwind", "Node", "Prisma", "PostgreSQL"],
-    desktopImage: "/assets/workshop-desktop.jpg",
-    mobileImage: null, // No mobile image provided in the HTML
-    link: "https://workshopbrad.netlify.app/",
-    repo: "https://github.com/bradmeyn/workshop",
-  },
-  {
-    title: "ClientBook",
-    description:
-      "A CRM for managing clients and related jobs. Server will take a bit to spin up as it's hosted for free.",
-    tech: ["Typescript", "Bootstrap", "Node", "MongoDB"],
-    desktopImage: "/assets/clientbook-desktop.jpg",
-    mobileImage: "/assets/clientbook-mobile.jpg",
-    link: "https://clientbook.onrender.com/",
-    repo: "https://github.com/bradmeyn/clientBook",
-  },
+
   {
     title: "Trackflix",
-    description: "Track movies you've watched and want to watch. ",
-
+    description:
+      "A project to learn React Server Components. Track movies you've watched and want to watch. ",
     tech: [
       "NextJS",
       "Tailwind",
@@ -56,5 +39,25 @@ export const projects = [
     mobileImage: "/assets/trackflix-mobile.jpg",
     link: "https://bradmeyn-trackflix.vercel.app/",
     repo: "https://github.com/bradmeyn/trackflix",
+    featured: true,
+  },
+  {
+    title: "ClientBook",
+    description:
+      "My first full-stack app. A CRM for managing clients and projects. Server will take a second to boot.",
+    tech: ["Typescript", "Bootstrap", "Node", "MongoDB"],
+    desktopImage: "/assets/clientbook-desktop.jpg",
+    mobileImage: "/assets/clientbook-mobile.jpg",
+    link: "https://clientbook.onrender.com/",
+    repo: "https://github.com/bradmeyn/clientBook",
+    featured: true,
+  },
+  {
+    title: "Drag & Drop",
+    description: "Testing dnd kit with a Trello-style drag & drop app.",
+    tech: ["React", "Tailwind", "Typescript", "Node"],
+    link: "https://bradmeyn-drag-and-drop.netlify.app",
+    repo: "https://github.com/bradmeyn/react-drag--drop",
+    featured: false,
   },
 ];
