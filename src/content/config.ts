0ec5f76c546e30a7b91b8a9ec5f9b0be6ddc0d9e
bradmeyn <bradjmeyn@gmail.com>
2024-07-28 20:09:12 +1000

    Add content dir and articles page

---
+++ src/content/config.ts
@@ -0,0 +1,27 @@
+import { defineCollection, z } from "astro:content";
+
+const articlesCollection = defineCollection({
+  type: "content",
+  schema: z.object({
+    isDraft: z.boolean(),
+    title: z.string(),
+    image: z.object({
+      src: z.string(),
+      alt: z.string(),
+    }).optional(),
+
+    tags: z.array(z.string()),
+    // An optional frontmatter property. Very common!
+    footnote: z.string().optional(),
+    // In frontmatter, dates written without quotes around them are interpreted as Date objects
+    publishDate: z.date(),
+    // You can also transform a date string (e.g. "2022-07-08") to a Date object
+    // publishDate: z.string().transform((str) => new Date(str)),
+    // Advanced: Validate that the string is also an email
+  }),
+});
+
+// This key should match your collection directory name in "src/content"
+export const collections = {
+  articles: articlesCollection,
+};
