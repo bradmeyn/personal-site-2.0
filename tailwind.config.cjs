1465ada0a37d732344861784141a600088ba8296
bradmeyn <bradjmeyn@gmail.com>
2024-08-11 18:58:16 +1000

    Add blog(hidden), simplify site significantly

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -6,6 +6,28 @@ module.exports = {
       container: {
         center: true,
         padding: "1rem",
+        screens: {
+          sm: "600px",
+        },
+      },
+      keyframes: {
+        fadeUp: {
+          from: { opacity: "0", transform: "translateY(50px)" },
+          to: { opacity: "1", transform: "translateY(0)" },
+        },
+        fadeDown: {
+          from: { opacity: "0", transform: "translateY(-30px)" },
+          to: { opacity: "1", transform: "translateY(0)" },
+        },
+        fadeRight: {
+          from: { opacity: "0", transform: "translateX(50px)" },
+          to: { opacity: "1", transform: "translateX(0)" },
+        },
+      },
+      animation: {
+        "fade-up": "fadeUp 0.6s ease-in-out forwards",
+        "fade-down": "fadeDown 0.6s ease-in-out forwards",
+        "fade-right": "fadeRight 0.6s ease-in-out forwards",
       },
     },
   },
