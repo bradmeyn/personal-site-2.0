77f09cb377523526e3d425f9b42e6fa87b312073
bradmeyn <bradjmeyn@gmail.com>
2025-05-30 17:26:19 +1000

    Update tailwind to use v4

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,19 +1,18 @@
 import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
-
-import tailwind from "@astrojs/tailwind";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://example.com",
   vite: {
-    resolve: {},
+    plugins: [tailwindcss()],
   },
   markdown: {
     shikiConfig: {
       wrap: true,
     },
   },
-  integrations: [mdx(), sitemap(), tailwind()],
+  integrations: [mdx(), sitemap()],
 });
