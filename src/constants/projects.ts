3f325a481a6d57e7fd3f2c5d7071b826b25d9c68
bradmeyn <bradjmeyn@gmail.com>
2025-06-03 08:21:39 +1000

    Site design overhall

--- src/constants/projects.ts
+++ src/constants/projects.ts
@@ -1,16 +1,20 @@
 export type Project = {
   title: string;
   description: string;
+  image: string;
+  tech: string[];
   link: string;
   repository: string;
 };
 
-export const PROJECTS = [
+export const PROJECTS: Project[] = [
   {
     title: "MoneyKit",
+    image: "assets/images/projects/moneykit.jpg",
     description:
       "A collection of financial tools to help make financial planning easier.",
+    tech: ["TypeScript", "SvelteKit", "Tailwind", "Chart.js"],
     link: "https://moneykit.au",
-    repository: "https://github.com/bradmeyn/wealthkit",
+    repository: "https://github.com/bradmeyn/moneykit",
   },
 ];
