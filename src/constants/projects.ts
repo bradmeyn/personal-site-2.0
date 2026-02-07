14c6c4fe7b9719c82e923820f6b0a56d3589a955
bradmeyn <bradjmeyn@gmail.com>
2026-02-07 14:52:17 +1100

    Minor updates

--- src/constants/projects.ts
+++ src/constants/projects.ts
@@ -9,7 +9,6 @@ export type Project = {
 export const PROJECTS: Project[] = [
   {
     title: "MoneyKit",
-
     description:
       "A collection of financial tools to help make financial planning easier.",
     tech: ["TypeScript", "SvelteKit", "Tailwind", "Chart.js"],
@@ -20,14 +19,7 @@ export const PROJECTS: Project[] = [
     title: "Embark",
 
     description: "Trip itinerary with AI intergration I made to plan holidays.",
-    tech: [
-      "TypeScript",
-      "SvelteKit",
-      "Tailwind",
-      "OpenAI",
-      "Drizzle",
-      "PostgreSQL",
-    ],
+    tech: ["TypeScript", "SvelteKit", "Tailwind", "Drizzle", "PostgreSQL"],
     repository: "https://github.com/bradmeyn/embark",
   },
   {
@@ -35,22 +27,7 @@ export const PROJECTS: Project[] = [
 
     description:
       "A mock financial advisor CRM project I use to keep up to date with my React & .NET skills.",
-    tech: [
-      "TypeScript",
-      "React",
-      ".NET Core",
-      "Tailwind",
-      "Entity Framework",
-      "Tanstack Query",
-    ],
+    tech: ["TypeScript", "React", ".NET", "Tailwind", "Tanstack Query"],
     repository: "https://github.com/bradmeyn/crm-api",
-  },
-  {
-    title: "Portfolio Pro",
-
-    description:
-      "A tool I made to track capital gains on my investments and estimate tax liability.",
-    tech: ["TypeScript", "SvelteKit", "Tailwind", "Drizzle", "PostgreSQL"],
-    repository: "https://github.com/bradmeyn/portfolio-pro",
   },
 ];
