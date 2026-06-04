0acb576fdc599693530d6d7703a4216db6cd4726
bradmeyn <bradjmeyn@gmail.com>
2026-06-04 14:28:02 +1000

    Refactor project and post components: remove unused LinkButton imports, update project details, and enhance styling for better readability

--- src/constants/projects.ts
+++ src/constants/projects.ts
@@ -10,9 +10,9 @@ export const PROJECTS: Project[] = [
   {
     title: "MoneyKit",
     description:
-      "A collection of financial tools to help make financial planning easier.",
-    tech: ["TypeScript", "SvelteKit", "Tailwind", "Chart.js"],
-    link: "https://moneykit.au",
+      "A collection of financial tools & a personal dashboard to track finances.",
+    tech: ["TypeScript", "SvelteKit", "Tailwind", "Postgresql"],
+
     repository: "https://github.com/bradmeyn/moneykit",
   },
   {
@@ -23,11 +23,10 @@ export const PROJECTS: Project[] = [
     repository: "https://github.com/bradmeyn/embark",
   },
   {
-    title: "CRM",
-
+    title: "Sage CRM",
     description:
-      "A mock financial advisor CRM project I use to keep up to date with my React & .NET skills.",
-    tech: ["TypeScript", "React", ".NET", "Tailwind", "Tanstack Query"],
-    repository: "https://github.com/bradmeyn/crm-api",
+      "A mock financial advisor CRM project I made to try out TanStack Start (cutting edge React framework)",
+    tech: ["TypeScript", "React", "TanStack", "Shadcn"],
+    repository: "https://github.com/bradmeyn/sage-crm",
   },
 ];
